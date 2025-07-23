e0ed3305fb7f912192324d5309eeb72452c66c97
minju <uy7396@gmail.com>
2025-07-23 18:01:24 +0900

    스타일 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Header from './include/Header';
-import Footer from './include/Footer';
-import Nav from './include/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/default.scss';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+import Nav from './layout/Nav';
 
 function App() {
   return (
     <>
     <Header/>
-    <Nav/>
-    <Footer/>
     </>
   );
 }
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,30 +1,65 @@
 import {Button, Container, Nav, Navbar, NavDropdown, Offcanvas} from 'react-bootstrap';
+import {  IoPawOutline, IoPaw } from "react-icons/io5";
+import {IconType} from 'react-icons';
+
+const PawIcon = IoPawOutline as IconType;
 
 const Header = () => {
   return(
-    <> 
-    <Navbar expand="sm" className="bg-body-tertiary mb-3">
+    <>
+    <Navbar expand="md">
       <Container fluid>
-        <Navbar.Brand href="#">Logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar"/>
-        
+        <Navbar.Brand href="/">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basicNav"/>
+
         <Navbar.Offcanvas
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNabarLabel"
+          id="basicNav"
+          aria-labelledby="basicLabel"
           placement="end">
 
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel"
+            <Offcanvas.Title id="basicLabel"
             >Logo</Offcanvas.Title>
           </Offcanvas.Header>
 
           <Offcanvas.Body>
-            <Nav className="justify-content-end  flex-grow-1 pe-3"></Nav>
-            내용을 작성합니다.
+            <Nav>
+              <NavDropdown
+                id="absicLabel"
+                title="동물을 찾습니다">
+                <NavDropdown.Item href="#">실종동물 찾습니다</NavDropdown.Item>
+                <NavDropdown.Item href="#">주인을 찾습니다</NavDropdown.Item>
+                <NavDropdown.Item href="#">상봉 완료!</NavDropdown.Item>
+              </NavDropdown>
+
+              <NavDropdown
+                id="absicLabel"
+                title="무료 분양">
+                <NavDropdown.Item href="#">분양합니다</NavDropdown.Item>
+                <NavDropdown.Item href="#">안전 분양 완료!</NavDropdown.Item>
+              </NavDropdown>
+
+              <NavDropdown
+                id="absicLabel"
+                title="봉사/후원하기">
+                <NavDropdown.Item href="#">자원봉사 모집</NavDropdown.Item>
+                <NavDropdown.Item href="#">기부&후원 요청</NavDropdown.Item>
+                <NavDropdown.Item href="#">나눔합니다</NavDropdown.Item>
+              </NavDropdown>
+
+              <NavDropdown
+                id="absicLabel"
+                title="커뮤니티">
+                <NavDropdown.Item href="#">공지사항</NavDropdown.Item>
+                <NavDropdown.Item href="#">반려동물 이야기</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
       </Container>
     </Navbar>
+
+    <a href="http://news.com">text</a>
     </>
   )
 };

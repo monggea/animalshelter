62777d6bd8ba28126e2d6de5ae42ddedb0fb56aa
minju <uy7396@gmail.com>
2025-07-30 14:39:25 +0900

    공지 게시판 컴포넌트 작성

--- src/pages/notice/Notice.tsx
+++ src/pages/notice/Notice.tsx
@@ -4,28 +4,47 @@ import { Badge } from "react-bootstrap";
 const Notice =()=>{
     return(
         <>
-        <ul className={`d-flex bg-color btn-custom ${styles.header}`}>
-            <li className="col-1 d-flex align-items-center justify-content-center">No</li>
-            <li className="col-7 d-flex align-items-center justify-content-center">제목</li>
-            <li className="col-2 d-flex align-items-center justify-content-center">작성일</li>
-            <li className="col-2 d-flex align-items-center justify-content-center">조회수</li>
-        </ul>
+        <section>
+            <header className={`d-flex bg-color btn-custom ${styles.header}`}>
+                <span className="no col-1 d-flex align-items-center justify-content-center">No</span>
+                <span className="title col-7 d-flex align-items-center justify-content-center">제목</span>
+                <span className="date col-2 d-flex align-items-center justify-content-center">작성일</span>
+                <span className="views col-2 d-flex align-items-center justify-content-center">조회수</span>
+            </header>
+        
+            <ul><li>
+                <article
+                    className={`d-flex ${styles.content}`}
+                    aria-labelledby="notice-title">
 
-        <ul className={`d-flex ${styles.content}`}>
-            <li className="col-1 d-flex align-items-center justify-content-center btn-custom">7</li>
+                    <span className="no col-1 d-flex align-items-center justify-content-center btn-custom">1</span>
+                    
+                    <div className="title col-7 d-flex align-items-center body-large gap-3">
+                        <h6 className={styles.title}>종합유기견보호센터의 새 모바일웹&어플이 출시되었습니다.</h6>
+                        
+                        <Badge
+                            aria-label="새로운 글"
+                            className={`secondary-bg tab ${styles.newTag}`}
+                            >NEW!
+                        </Badge>
+                    </div>
 
-            <li className="col-7 d-flex align-items-center body-large gap-3">
-                <span className={styles.title}>종합유기견보호센터의 새 모바일웹&어플이 출시되었습니다.</span>
-                <Badge className={`secondary-bg tab ${styles.newTag}`}>NEW!</Badge>
-            </li>
+                    <time
+                        dateTime="2018-10-06"
+                        className="date col-2 d-flex align-items-center justify-content-center text-dark"
+                        aria-label="작성일 2018년 10월 6일"
+                        >2018-10-06
+                    </time>
 
-            <li className="col-2 d-flex align-items-center justify-content-center text-dark">2018-10-06</li>
-
-            <li className="col-2 d-flex align-items-center justify-content-center text-dark gap-2">
-                <div className={styles.icon}></div>
-                19873
-            </li>
-        </ul>
+                    <span className="views col-2 d-flex align-items-center justify-content-center text-dark gap-2">
+                        <div aria-hidden="true" className={styles.icon}></div>
+                        19873
+                    </span>
+                    
+                </article>
+            </li></ul>
+        
+        </section>
         </>
     )
 };
